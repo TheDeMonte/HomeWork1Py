87ca72ea679bd33c87cedeacc0f6e124781a4c3d
Ivan Sidorenko <cidor.0712@gmail.com>
2022-11-22 01:52:52 +0500

    Task5

--- Task5.py
+++ Task5.py
@@ -4,12 +4,16 @@
 # - A (3,6); B (2,1) -> 5.09
 # - A (7,-5); B (1,-1) -> 7.21
 import math
-def Distance(x1,y1,x2,y2):
+
+
+def Distance(x1, y1, x2, y2):
     if (x1 == x2 and y1 == y2):
         return 0
     numX = x1 - x2
     numY = y1 - y2
-    return round(math.sqrt(numX*numX + numY*numY),3)
+    return round(math.sqrt(numX*numX + numY*numY), 3)
+
+
 print("Введите координаты первой точки")
 print("X: ")
 x1 = float(input())
@@ -21,5 +25,5 @@ x2 = float(input())
 print("Y: ")
 y2 = float(input())
 
-dist = Distance(x1,y1,x2,y2)
+dist = Distance(x1, y1, x2, y2)
 print(f"Расстояние между точками равно {dist}")
